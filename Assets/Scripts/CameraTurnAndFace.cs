ae28bd1aaeb2d2fd1d0572e4f4c2f9120448f2b7
whatPanache <pwcarney@bu.edu>
2018-01-06 20:04:15 -0500

    Done main menu mockup

--- Assets/Scripts/CameraTurnAndFace.cs
+++ Assets/Scripts/CameraTurnAndFace.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CameraTurnAndFace : MonoBehaviour
 {
@@ -8,6 +9,9 @@ public class CameraTurnAndFace : MonoBehaviour
     public GameObject start_button;
     public GameObject left_button;
     public GameObject right_button;
+    public GameObject play_button;
+    public GameObject pyramids_title;
+    public GameObject stonehenge_title;
 
     public Transform title_card;
     public Transform level1;
@@ -36,18 +40,47 @@ public class CameraTurnAndFace : MonoBehaviour
     public void StartGame()
     {
         ToggleTitle();
-        current_target = level1;
+        SelectLevel(1);
     }
 
     public void ChangeLevels(bool left)
     {
         if (current_target == level1)
         {
-            current_target = level2;
+            SelectLevel(2);
         }
         else
         {
-            current_target = level1;
+            SelectLevel(1);
+        }
+    }
+
+    private void SelectLevel(int selected)
+    {
+        switch (selected)
+        {
+            case 1:
+                current_target = level1;
+                pyramids_title.SetActive(true);
+                stonehenge_title.SetActive(false);
+                break;
+            case 2:
+                current_target = level2;
+                stonehenge_title.SetActive(true);
+                pyramids_title.SetActive(false);
+                break;
+        }
+    }
+
+    public void PlayLevel()
+    {
+        if (current_target == level1)
+        {
+            SceneManager.LoadScene("Pyramids");
+        }
+        else if (current_target == level2)
+        {
+            SceneManager.LoadScene("Stonehenge");
         }
     }
 
@@ -57,5 +90,6 @@ public class CameraTurnAndFace : MonoBehaviour
         start_button.SetActive(false);
         left_button.SetActive(true);
         right_button.SetActive(true);
+        play_button.SetActive(true);
     }
 }
