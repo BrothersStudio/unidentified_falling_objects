058a3f022fdcbaf0254efc8aca35095e3c5f2cef
whatPanache <pwcarney@bu.edu>
2018-01-08 19:15:13 -0500

    Added sound framework

---
+++ Assets/Scripts/BoxSounds.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxSounds : MonoBehaviour
+{
+    bool played_sound = false;
+    AudioSource source;
+
+    private void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Ground")
+        {
+            Debug.Log("Play ground sound");
+            played_sound = true;
+        }
+        else if (other.tag == "Block")
+        {
+            Debug.Log("Play block sound");
+            played_sound = true;
+        }
+    }
+}
