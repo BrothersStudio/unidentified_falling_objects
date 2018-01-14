61bc2403a9feb3493a0cb5e218a062ff89570705
whatPanache <pwcarney@bu.edu>
2018-01-14 14:38:09 -0500

    Things grow and shrink at the same rate

---
+++ Assets/Scripts/GrowAndShrinkSingleton.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrowAndShrinkSingleton : MonoBehaviour
+{
+    private static GrowAndShrinkSingleton _instance;
+    public static GrowAndShrinkSingleton Instance
+    {
+        get
+        {
+            return _instance;
+        }
+    }
+
+    private static float scale = 1f;
+    float min_scale = 0.9f;
+    float max_scale = 1.1f;
+    float grow_speed = 0.01f;
+    bool growing = true;
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (growing && scale < max_scale)
+        {
+            scale += grow_speed;
+        }
+        else if (!growing && scale > min_scale)
+        {
+            scale -= grow_speed;
+        }
+
+        if (growing && scale >= max_scale)
+        {
+            growing = false;
+        }
+        else if (!growing && scale <= min_scale)
+        {
+            growing = true;
+        }
+    }
+
+    public static float Scale
+    {
+        get
+        {
+            return scale;
+        }
+    }
+}
--- Assets/Scripts/TextGrowAndShrink.cs
+++ Assets/Scripts/TextGrowAndShrink.cs
@@ -4,33 +4,13 @@ using UnityEngine;
 
 public class TextGrowAndShrink : MonoBehaviour
 {
-    float min_scale = 0.9f;
-    float max_scale = 1.1f;
-    float grow_speed = 0.01f;
-    bool growing = true;
-
-	void FixedUpdate ()
+    void FixedUpdate ()
     {
         Vector3 scale = GetComponent<RectTransform>().localScale;
-        if (growing && scale.x < max_scale)
-        {
-            scale.x += grow_speed;
-            scale.y += grow_speed;
-        }
-        else if (!growing && scale.x > min_scale)
-        {
-            scale.x -= grow_speed;
-            scale.y -= grow_speed;
-        }
-        GetComponent<RectTransform>().localScale = scale;
 
-        if (growing && scale.x >= max_scale)
-        {
-            growing = false;
-        }
-        else if (!growing && scale.x <= min_scale)
-        {
-            growing = true;
-        }
+        scale.x = GrowAndShrinkSingleton.Scale;
+        scale.y = GrowAndShrinkSingleton.Scale;
+
+        GetComponent<RectTransform>().localScale = scale;
     }
 }
