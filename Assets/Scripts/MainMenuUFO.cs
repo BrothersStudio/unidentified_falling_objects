055d889db7605482b768e9a7ada9b1aa3f05be1b
whatPanache <pwcarney@bu.edu>
2018-01-14 18:23:14 -0500

    Added flying UFO to main menu

---
+++ Assets/Scripts/MainMenuUFO.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuUFO : MonoBehaviour
+{
+    float speed = 4f;
+    float sine_place = 0f;
+    float sine_speed = 0.08f;
+    float sine_amp = 2.5f;
+    Vector3 starting_position;
+
+	void Start ()
+    {
+        starting_position = transform.localPosition;
+	}
+	
+	void FixedUpdate ()
+    {
+        Vector3 new_position = transform.localPosition;
+
+        new_position.x += Time.deltaTime * speed;
+        new_position.y += Time.deltaTime * Mathf.Sin(sine_place) * sine_amp;
+        sine_place += sine_speed;
+
+        transform.localPosition = new_position;
+
+        if (!GetComponentInChildren<Renderer>().isVisible)
+        {
+            Vector3 new_start = starting_position;
+            new_start.y += Random.Range(-1f, 1f);
+            transform.localPosition = new_start;
+        }
+    }
+}
