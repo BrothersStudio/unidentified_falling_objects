9d33e0e270888784a5945ede628d81b4766d5041
whatPanache <pwcarney@bu.edu>
2018-01-15 18:15:23 -0500

    Added world builder and tweaked main menu ufo

--- Assets/Scripts/MainMenuUFO.cs
+++ Assets/Scripts/MainMenuUFO.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class MainMenuUFO : MonoBehaviour
 {
-    float speed = 4f;
-    float sine_place = 0f;
-    float sine_speed = 0.08f;
-    float sine_amp = 2.5f;
+    float speed = 2f;
+    float sine_place_y = 0f;
+    float sine_speed_y = 0.08f;
+    float sine_amp_y = 2.5f;
+    float sine_place_z = 0f;
+    float sine_speed_z = 0.04f;
+    float sine_amp_z = 1f;
     Vector3 starting_position;
 
 	void Start ()
@@ -20,12 +23,14 @@ public class MainMenuUFO : MonoBehaviour
         Vector3 new_position = transform.localPosition;
 
         new_position.x += Time.deltaTime * speed;
-        new_position.y += Time.deltaTime * Mathf.Sin(sine_place) * sine_amp;
-        sine_place += sine_speed;
+        new_position.y += Time.deltaTime * Mathf.Sin(sine_place_y) * sine_amp_y;
+        sine_place_y += sine_speed_y;
+        new_position.z += Time.deltaTime * Mathf.Sin(sine_place_z) * sine_amp_z;
+        sine_place_z += sine_speed_z;
 
         transform.localPosition = new_position;
 
-        if (!GetComponentInChildren<Renderer>().isVisible)
+        if (new_position.x > 7.5)
         {
             Vector3 new_start = starting_position;
             new_start.y += Random.Range(-1f, 1f);
