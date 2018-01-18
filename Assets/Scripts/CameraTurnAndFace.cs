b249c7d486c7945287f5363ddb2e25e596830c14
whatPanache <pwcarney@bu.edu>
2018-01-17 22:14:09 -0500

    Added every level to main menu, new level selection motion

--- Assets/Scripts/CameraTurnAndFace.cs
+++ Assets/Scripts/CameraTurnAndFace.cs
@@ -20,18 +20,28 @@ public class CameraTurnAndFace : MonoBehaviour
     public GameObject stonehenge_title;
     public GameObject eiffel_title;
     public GameObject pisa_title;
+    public GameObject needle_title;
+    public GameObject taj_title;
+    public GameObject golden_gate_title;
+    public GameObject babel_title;
 
     public Transform title_card;
     public Transform level1;
     public Transform level2;
     public Transform level3;
     public Transform level4;
+    public Transform level5;
+    public Transform level6;
+    public Transform level7;
+    public Transform level8;
     Transform current_target;
 
+    Vector3 orig_position;
     Vector3 direction;
     Quaternion lookRotation;
 
-    float turn_speed = 4f;
+    float move_speed = 30f;
+    float turn_speed = 8f;
 
     // Scores
     private int current_level = 0;
@@ -43,6 +53,7 @@ public class CameraTurnAndFace : MonoBehaviour
     private void Start()
     {
         current_target = title_card;
+        orig_position = transform.position;
 
         score_holder = score_table.transform.Find("Score Holder");
     }
@@ -79,6 +90,11 @@ public class CameraTurnAndFace : MonoBehaviour
         lookRotation = Quaternion.LookRotation(direction);
 
         transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * turn_speed);
+
+        if (Quaternion.Angle(transform.rotation, lookRotation) < 10 && Vector3.Distance(transform.position, current_target.position) > 10f)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, current_target.position, Time.deltaTime * move_speed);
+        }
 	}
 
     public void StartGame()
@@ -99,7 +115,7 @@ public class CameraTurnAndFace : MonoBehaviour
         {
             if (current_target == level1)
             {
-                SelectLevel("Pisa");
+                SelectLevel("Babel");
             }
             else if (current_target == level2)
             {
@@ -112,6 +128,22 @@ public class CameraTurnAndFace : MonoBehaviour
             else if (current_target == level4)
             {
                 SelectLevel("EiffelTower");
+            }
+            else if (current_target == level5)
+            {
+                SelectLevel("Pisa");
+            }
+            else if (current_target == level6)
+            {
+                SelectLevel("SpaceNeedle");
+            }
+            else if (current_target == level7)
+            {
+                SelectLevel("Taj");
+            }
+            else if (current_target == level8)
+            {
+                SelectLevel("GoldenGate");
             }
         }
         else // Right
@@ -130,6 +162,22 @@ public class CameraTurnAndFace : MonoBehaviour
             }
             else if (current_target == level4)
             {
+                SelectLevel("SpaceNeedle");
+            }
+            else if (current_target == level5)
+            {
+                SelectLevel("Taj");
+            }
+            else if (current_target == level6)
+            {
+                SelectLevel("GoldenGate");
+            }
+            else if (current_target == level7)
+            {
+                SelectLevel("Babel");
+            }
+            else if (current_target == level8)
+            {
                 SelectLevel("Pyramids");
             }
         }
@@ -138,40 +186,65 @@ public class CameraTurnAndFace : MonoBehaviour
     public void SelectLevel(string selected)
     {
         ToggleTitle();
+        transform.position = orig_position;
 
         switch (selected)
         {
             case "Pyramids":
                 current_level = 1;
                 current_target = level1;
+                babel_title.SetActive(false);
                 pyramids_title.SetActive(true);
                 stonehenge_title.SetActive(false);
-                eiffel_title.SetActive(false);
-                pisa_title.SetActive(false);
                 break;
             case "Stonehenge":
                 current_level = 2;
                 current_target = level2;
-                stonehenge_title.SetActive(true);
                 pyramids_title.SetActive(false);
+                stonehenge_title.SetActive(true);
                 eiffel_title.SetActive(false);
-                pisa_title.SetActive(false);
                 break;
             case "EiffelTower":
                 current_level = 3;
                 current_target = level3;
-                eiffel_title.SetActive(true);
-                pyramids_title.SetActive(false);
                 stonehenge_title.SetActive(false);
+                eiffel_title.SetActive(true); 
                 pisa_title.SetActive(false);
                 break;
             case "Pisa":
                 current_level = 4;
                 current_target = level4;
-                pisa_title.SetActive(true);
                 eiffel_title.SetActive(false);
+                pisa_title.SetActive(true);
+                needle_title.SetActive(false);
+                break;
+            case "SpaceNeedle":
+                current_level = 5;
+                current_target = level5;
+                pisa_title.SetActive(false);
+                needle_title.SetActive(true);
+                taj_title.SetActive(false);
+                break;
+            case "Taj":
+                current_level = 6;
+                current_target = level6;
+                needle_title.SetActive(false);
+                taj_title.SetActive(true);
+                golden_gate_title.SetActive(false);
+                break;
+            case "GoldenGate":
+                current_level = 7;
+                current_target = level7;
+                taj_title.SetActive(false);
+                golden_gate_title.SetActive(true);
+                babel_title.SetActive(false);
+                break;
+            case "Babel":
+                current_level = 8;
+                current_target = level8;
+                golden_gate_title.SetActive(false);
+                babel_title.SetActive(true);
                 pyramids_title.SetActive(false);
-                stonehenge_title.SetActive(false);
                 break;
         }
         ClearLeaderboard();
@@ -195,6 +268,22 @@ public class CameraTurnAndFace : MonoBehaviour
         else if (current_target == level4)
         {
             SceneManager.LoadScene("Pisa");
+        }
+        else if (current_target == level5)
+        {
+            SceneManager.LoadScene("SpaceNeedle");
+        }
+        else if (current_target == level6)
+        {
+            SceneManager.LoadScene("Taj");
+        }
+        else if (current_target == level7)
+        {
+            SceneManager.LoadScene("GoldenGate");
+        }
+        else if (current_target == level8)
+        {
+            //SceneManager.LoadScene("Babel");
         }
     }
 
