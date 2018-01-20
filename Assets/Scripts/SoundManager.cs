3b717cd7e8c6ef421599f0a4c9941bc2ec753df3
kevincarney <krcarney@vt.edu>
2018-01-20 12:16:19 -0500

    added sound manager

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+public class SoundManager : MonoBehaviour {
+
+    public AudioSource fxSource;
+    public AudioSource musicSource;
+    public static SoundManager instance = null;
+    public AudioClip inLevelMusic;
+    public AudioClip menuMusic;
+    public AudioMixerSnapshot Mute;
+
+    private int fadeOutTime = 2;
+    private int fadeInTime = 2;
+
+    private void OnEnable()
+    {
+        SceneManager.activeSceneChanged += CheckChange;
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.activeSceneChanged += CheckChange;
+    }
+
+    void CheckChange(Scene previous, Scene next)
+    {
+        if (next.name == "MainMenu")
+        {
+            PlayMusic(menuMusic);
+        }
+        else
+        {
+            PlayMusic(inLevelMusic);
+        }
+    }
+
+    void Awake ()
+    {
+
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+	}
+
+    public void PlaySingleFx(AudioClip clip)
+    {
+        fxSource.clip = clip;
+        fxSource.Play();
+    }
+
+    public void PlayMusic(AudioClip clip)
+    {
+        musicSource.clip = clip;
+        musicSource.Play();
+    }
+
+    IEnumerator FadeInMusic()
+    {
+        float t = fadeInTime;
+
+        while (t > 0)
+        {
+            yield return null;
+            t -= Time.deltaTime;
+            musicSource.volume = (1 - t / fadeInTime);
+        }
+        yield break;
+    }
+
+
+    IEnumerator FadeOutMusic()
+    {
+        float t = fadeOutTime;
+        while (t > 0)
+        {
+            yield return null;
+            t -= Time.deltaTime;
+            musicSource.volume = t / fadeOutTime;
+        }
+        yield break;
+    }
+}
