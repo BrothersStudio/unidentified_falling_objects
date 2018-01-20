d71905345b487296166f0d6fd65c3f81e8a47a0e
whatPanache <pwcarney@bu.edu>
2018-01-20 12:44:53 -0500

    Big end of level improvements

---
+++ Assets/Scripts/NextLevel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+public class NextLevel : MonoBehaviour
+{
+    public void NextScene()
+    {
+        string current_name = SceneManager.GetActiveScene().name;
+        int next_ind = SceneIndices.GetIndex(current_name) + 1;
+        string next_name = SceneIndices.GetName(next_ind);
+        SceneManager.LoadScene(next_name);
+    }
+}
