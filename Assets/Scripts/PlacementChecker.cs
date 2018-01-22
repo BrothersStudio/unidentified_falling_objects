a72e9ac9585aa5640c1b92e025d9eb80f6f71277
whatPanache <pwcarney@bu.edu>
2018-01-21 22:58:18 -0500

    Restart text less aggressive

--- Assets/Scripts/PlacementChecker.cs
+++ Assets/Scripts/PlacementChecker.cs
@@ -196,7 +196,7 @@ public class PlacementChecker : MonoBehaviour
                         SpawnScoreWord(added_score, blocks[i].transform.position);
 
                         // Check for pity reset text
-                        if (blocks_dropped > 1 && (score / (score_factor * blocks_dropped) < 0.5))
+                        if (blocks_dropped > 1 && (score / (score_factor * blocks_dropped) < 0.3))
                         {
                             reset_text.SetActive(true);
                         }
