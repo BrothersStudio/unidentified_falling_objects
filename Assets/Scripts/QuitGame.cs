e2170553634fe6c7e67a2c393147c1c02c60f02a
whatPanache <pwcarney@bu.edu>
2018-01-21 23:51:51 -0500

    Tweaked anchors

---
+++ Assets/Scripts/QuitGame.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitGame : MonoBehaviour
+{
+	void Update ()
+    {
+        if (Input.GetKey(KeyCode.Escape) && Time.timeSinceLevelLoad > 1f)
+        {
+            //Application.Quit();
+        }
+    }
+}
