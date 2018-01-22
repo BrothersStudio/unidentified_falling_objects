a6ad89887b0d1c4e96d18526c6248d2c7285ce31
whatPanache <pwcarney@bu.edu>
2018-01-22 01:09:38 -0500

    Changed header name

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -14,7 +14,7 @@ public class SoundManager : MonoBehaviour {
 
     private int trackNumber = 0;
     private float lengthOfClip;
-    public float numOfRepeats = 2;
+    public float numOfRepeats;
     private float timeOfRepeats;
     private float fadeOutTime = 15;
     private float fadeInTime = 10;
